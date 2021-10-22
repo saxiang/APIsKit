ea3279dc0d809e5c130d81300685a02858ed22fe
chenzhanxiang <chenzhanxiang@phoenixtv.com>
2021-10-22 11:00:57 +0800

    版本8

--- APIsKit.podspec
+++ APIsKit.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'APIsKit'
-  s.version          = '7'
+  s.version          = '8'
   s.summary          = 'A short description of APIsKit.'
 
 # This description is used to generate tags and improve search results.
--- APIsKit/Classes/APIsCommonOC.m
+++ APIsKit/Classes/APIsCommonOC.m
@@ -6,14 +6,15 @@
 //
 
 #import "APIsCommonOC.h"
-#import "UIImage+common.h"
+#import "cheungKitTempTest.h"
 
 @implementation APIsCommonOC
 +(void)testDependency_showImgInView:(UIView *)view andFrame:(CGRect)frame {
-    UIImage *testImg = [UIImage fs_imagePathWithName:@"IMG_1134" bundle:@"APIsKit" targetClass:[self class]];
-    UIImageView *imgView = [[UIImageView alloc] initWithImage:testImg];
-    imgView.backgroundColor = UIColor.redColor;
-    imgView.frame = frame;
-    [view addSubview:imgView];
+//    UIImage *testImg = [UIImage fs_imagePathWithName:@"IMG_1134" bundle:@"APIsKit" targetClass:[self class]];
+//    UIImageView *imgView = [[UIImageView alloc] initWithImage:testImg];
+//    imgView.backgroundColor = UIColor.redColor;
+//    imgView.frame = frame;
+//    [view addSubview:imgView];
+    [cheungKitTempTest testCall];
 }
 @end
